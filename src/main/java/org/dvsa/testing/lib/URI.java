3a1a36ce3af12ea9d7450ff648a673d0cc0cc5bb
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-01-05 16:05:32 +0000

    Create URI#updatePath for changing the path of a URL without generating the whole thing

--- src/main/java/org/dvsa/testing/lib/URI.java
+++ src/main/java/org/dvsa/testing/lib/URI.java
@@ -2,6 +2,10 @@ package org.dvsa.testing.lib;
 
 import org.dvsa.testing.lib.utils.ApplicationType;
 import org.dvsa.testing.lib.utils.EnvironmentType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class URI {
     private static String scheme = "https";
@@ -20,6 +24,20 @@ public class URI {
     public static String build(ApplicationType appType, EnvironmentType env){
         String endPoint = "";
         return URI.build(appType, env, endPoint);
+    }
+
+    public static String updatePath(@NotNull String URL, @NotNull String path){
+        String regex = "(?:(?<=\\.aws\\/)|(?<=\\.uk\\/))[\\w\\/]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(URL);
+
+        if(matcher.find()){
+            return URL.replaceAll(regex, path);
+        } else {
+            path = URL.endsWith("/") ? URL + path : URL + "/" + path;
+        }
+
+        return URL.replaceAll(regex, path);
     }
 
     private static String appName(ApplicationType appType){
