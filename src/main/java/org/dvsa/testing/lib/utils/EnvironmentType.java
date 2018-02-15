f75b90f40c20274e7dd9ad65ae407f91733e5af8
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-02-15 11:09:50 +0000

    Add string representations of each EnvironmentType enum type

--- src/main/java/org/dvsa/testing/lib/utils/EnvironmentType.java
+++ src/main/java/org/dvsa/testing/lib/utils/EnvironmentType.java
@@ -1,10 +1,23 @@
 package org.dvsa.testing.lib.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum EnvironmentType {
-    QUALITY_ASSURANCE,
-    DAILY_ASSURANCE,
-    DEVELOP,
-    REGRESSION,
-    INTEGRATION,
-    PRODUCTION,
+    QUALITY_ASSURANCE("qa"),
+    DAILY_ASSURANCE("da"),
+    DEVELOP("dev"),
+    REGRESSION("reg"),
+    INTEGRATION("int"),
+    PRODUCTION("prod");
+
+    private String value;
+
+    EnvironmentType(@NotNull String name) {
+        this.value = name;
+    }
+
+    @Override
+    public final String toString() {
+        return this.value;
+    }
 }
