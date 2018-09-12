9d35b815466464d7520ab1e5269f92c620754905
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-09-12 13:29:09 +0100

    Remove checked exception from method signature

--- src/main/java/org/dvsa/testing/lib/url/database/URL.java
+++ src/main/java/org/dvsa/testing/lib/url/database/URL.java
@@ -11,8 +11,8 @@ public class URL extends URLBase {
     private static String DEFAULT_SCHEMA = "OLCS_RDS_OLCSDB";
     private static int DEFAULT_PORT = 3306;
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment, @NotNull String schema, int port) {
-        String URL = String.format(
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment, @NotNull String schema, int port) {
+        return String.format(
                 // TODO: Investigate prod structure, this only supports nonprod VPC for now.
                 "jdbc:mysql://olcsdb-rds.olcs.%s.nonprod.dvsa.aws:%d/%s?user=%s&password=%s&useSSL=false",
                 environment.toString(),
@@ -21,36 +21,33 @@ public class URL extends URLBase {
                 DBUsername,
                 DBPassword
         );
-
-        setURL(URL);
-        return getURL();
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment, int port) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment, int port) {
         return build(DBUsername, DBPassword, environment, DEFAULT_SCHEMA, port);
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment, @NotNull String schema) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment, @NotNull String schema) {
         return build(DBUsername, DBPassword, environment, schema, DEFAULT_PORT);
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, EnvironmentType environment) {
         return build(DBUsername, DBPassword, environment, DEFAULT_SCHEMA, DEFAULT_PORT);
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment, @NotNull String schema, int port) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment, @NotNull String schema, int port) {
         return build(DBUsername, DBPassword, EnvironmentType.getEnum(environment), schema, port);
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment, int port) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment, int port) {
         return build(DBUsername, DBPassword, EnvironmentType.getEnum(environment), port);
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment, @NotNull String schema) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment, @NotNull String schema) {
         return build(DBUsername, DBPassword, EnvironmentType.getEnum(environment), schema, DEFAULT_PORT);
     }
 
-    public static java.net.URL build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment) {
+    public static String build(@NotNull String DBUsername, @NotNull String DBPassword, @NotNull String environment) {
         return build(DBUsername, DBPassword, EnvironmentType.getEnum(environment));
     }
 
