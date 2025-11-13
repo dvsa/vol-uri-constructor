de702d7cb6f4dc67e95791a6593010594dd9715f
Stefan Ranoszek <stefan48500@gmail.com>
2025-11-13 12:15:15 +0000

    fix: local setup urls

--- src/main/java/org/dvsa/testing/lib/url/api/ApiUrl.java
+++ src/main/java/org/dvsa/testing/lib/url/api/ApiUrl.java
@@ -7,9 +7,9 @@ import org.jetbrains.annotations.NotNull;
 public class ApiUrl extends URLBase {
 
     public static java.net.URL build(@NotNull String scheme, @NotNull EnvironmentType environment, @NotNull String path) {
-        var baseUrl= "";
+        var baseUrl = "";
         if (environment == EnvironmentType.LOCAL) {
-            baseUrl = String.format("%s://olcs-backend/api/%s", scheme, path);
+            baseUrl = String.format("%s://api.local.olcs.dev-dvsacloud.uk/api/%s", scheme, path);
         } else {
             baseUrl = String.format("%s://api.%s.olcs.dev-dvsacloud.uk/api/%s", scheme, environment, path);
             if (environment == EnvironmentType.PRODUCTION) {
